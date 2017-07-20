fa2c13dbe5a6f7ccb8ec8f5d588bb7e7b056f66e
Greg Fauconnier <g.fauconnier@hotmail.fr>
2017-07-20 14:00:01 +0200

    main.css : changed font-size for better lisibility - main.js : added a function to format time + make the type of step change when 0second is reached

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
 var breakTime = 5;
 var sessionTime = 20;
-var stepType = "Session";
+var stepType = ["Session", "Break"];
 var stopped = true;
+var intervalPom;
+var intervalTime = sessionTime;
+
 
 $("#breakP").text("Break time : " + breakTime);
 $("#sessionP").text("Session time : " + sessionTime);
-$("#step").text(stepType);
-$("#chronoP").text(sessionTime);
+$("#step").text(stepType[0]);
+$("#chronoP").text(formatTime(sessionTime));
 
 $(".breakbtn").click(function() {
   if (stopped) {
-    if ($(".breakbtn").index(this) == 0) {
+    if ($(".breakbtn").index(this) == 0 && breakTime != 1) {
       breakTime--;
-    } else {
+    } else if ($(".breakbtn").index(this) == 1) {
       breakTime++;
     }
     $("#breakP").text("Break time : " + breakTime);
@@ -21,11 +24,41 @@ $(".breakbtn").click(function() {
 
 $(".sessionbtn").click(function() {
   if (stopped) {
-    if ($(".sessionbtn").index(this) == 0) {
+    if ($(".sessionbtn").index(this) == 0 && sessionTime != 1) {
       sessionTime--;
-    } else {
+    } else if ($(".sessionbtn").index(this) == 1){
       sessionTime++;
     }
     $("#sessionP").text("Session time : " + sessionTime);
   }
 });
+
+$(".chrono").click(function() {
+  if (stopped) {
+    stepType[0] == "Session" ? intervalTime = sessionTime : intervalTime = breakTime;
+    intervalPom = setInterval(chronoPomo, 1000);
+    stopped = false;
+  } else {
+    stopped = true;
+    clearInterval(intervalPom);
+  }
+});
+
+function chronoPomo() {
+  if (intervalTime) {
+    intervalTime--;
+    $("#chronoP").text(formatTime(intervalTime));
+  }
+  else {
+    stepType.push(stepType.shift());
+    stepType[0] == "Session" ? intervalTime = sessionTime : intervalTime = breakTime;
+    $("#step").text(stepType[0]);
+    $("#chronoP").text(formatTime(intervalTime));
+  }
+}
+
+
+function formatTime(fTime) {
+  fTime = ("0" + Math.floor(intervalTime / 60)).slice(-2) + " : " + ("0" + intervalTime % 60).slice(-2);
+  return fTime;
+}
