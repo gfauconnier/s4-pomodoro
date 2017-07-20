57ccdc5d0f0b6ba4965a67fe28cc94bedd2ffb0c
Greg Fauconnier <g.fauconnier@hotmail.fr>
2017-07-20 11:21:54 +0200

    main.js : added click event on buttons to change the timers if the timer is stopped

--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
 var breakTime = 5;
 var sessionTime = 20;
 var stepType = "Session";
+var stopped = true;
 
 $("#breakP").text("Break time : " + breakTime);
 $("#sessionP").text("Session time : " + sessionTime);
 $("#step").text(stepType);
 $("#chronoP").text(sessionTime);
+
+$(".breakbtn").click(function() {
+  if (stopped) {
+    if ($(".breakbtn").index(this) == 0) {
+      breakTime--;
+    } else {
+      breakTime++;
+    }
+    $("#breakP").text("Break time : " + breakTime);
+  }
+});
+
+$(".sessionbtn").click(function() {
+  if (stopped) {
+    if ($(".sessionbtn").index(this) == 0) {
+      sessionTime--;
+    } else {
+      sessionTime++;
+    }
+    $("#sessionP").text("Session time : " + sessionTime);
+  }
+});
