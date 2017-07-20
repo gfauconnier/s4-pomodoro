38c503e5df6fe8aa21efca393860220e073742a0
Greg Fauconnier <g.fauconnier@hotmail.fr>
2017-07-20 11:14:04 +0200

    index.html : added divs for display purpose - main.css : positioned everything (at least for desktop) - main.js : added dom to display default values

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 var breakTime = 5;
 var sessionTime = 20;
+var stepType = "Session";
 
-$("#breakP").text(breakTime);
+$("#breakP").text("Break time : " + breakTime);
+$("#sessionP").text("Session time : " + sessionTime);
+$("#step").text(stepType);
+$("#chronoP").text(sessionTime);
