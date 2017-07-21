ce7fbbaadc2067bef741cfd7cbd1a1abd4f19b89
Greg Fauconnier <g.fauconnier@hotmail.fr>
2017-07-21 09:19:24 +0200

    main.css : changed a few declarations - main.js : tested animations with jquery

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var breakTime = 5;
 var sessionTime = 20;
 var stepType = ["Session", "Break"];
 var stopped = true;
+var firstStep = true;
 var intervalPomo;
 var intervalTime = sessionTime;
 
@@ -26,7 +27,7 @@ $(".sessionbtn").click(function() {
   if (stopped) {
     if ($(".sessionbtn").index(this) == 0 && sessionTime != 1) {
       sessionTime--;
-    } else if ($(".sessionbtn").index(this) == 1){
+    } else if ($(".sessionbtn").index(this) == 1) {
       sessionTime++;
     }
     $("#sessionP").text("Session time : " + sessionTime);
@@ -35,8 +36,14 @@ $(".sessionbtn").click(function() {
 
 $(".chrono").click(function() {
   if (stopped) {
+    if (firstStep) {
+      intervalTime = sessionTime;
+      $("#chronoP").text(formatTime(intervalTime));
+    }
+    firstStep = false;
     stopped = false;
     intervalPomo = setInterval(chronoPomo, 1000);
+    animation();
   } else {
     stopped = true;
     clearInterval(intervalPomo);
@@ -47,16 +54,20 @@ function chronoPomo() {
   if (intervalTime) {
     intervalTime--;
     $("#chronoP").text(formatTime(intervalTime));
-  }
-  else {
+  } else {
     stepType.push(stepType.shift());
     stepType[0] == "Session" ? intervalTime = sessionTime : intervalTime = breakTime;
     $("#step").text(stepType[0]);
     $("#chronoP").text(formatTime(intervalTime));
+    animation();
   }
 }
 
 function formatTime(fTime) {
   fTime = ("0" + Math.floor(fTime / 60)).slice(-2) + " : " + ("0" + fTime % 60).slice(-2);
   return fTime;
+}
+
+function animation() {
+  stepType[0] == "Session" ? $(".filler").animate({"left" : "27.5vw"}, intervalTime * 1000) : $(".filler").animate({"left" : "-7.5vw"}, intervalTime * 1000);
 }
